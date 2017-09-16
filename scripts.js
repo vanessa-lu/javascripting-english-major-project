aa4d99806379cb20a78c319526e5a3c5d4526d95
Vanessa Lu <vanessalu@Vanessas-MacBook-Pro.local>
2017-09-16 11:38:52 -0400

    Prompt Answer

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
-alert("scripts.js has loaded!");
+let userInput;
+userInput = prompt("What do you want to have for dinner?", "Type your answer here.");
+if ( userInput === "burrito" ) {
+  $("#response").html("Brilliant choice!");
+} else {
+  $("#response").html("Don’t you want a burrito?");
+}
