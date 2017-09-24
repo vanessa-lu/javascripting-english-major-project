b68b02c6d3e93e4266defe325b006d5cc53ffce9
Vanessa Lu <vanessalu@Vanessas-MacBook-Pro.local>
2017-09-24 10:44:04 -0400

    tip calculator

--- scripts.js
+++ scripts.js
@@ -1,7 +1,13 @@
-let userInput;
-userInput = prompt("What do you want to have for dinner?", "Type your answer here.");
-if ( userInput === "burrito" ) {
-  $("#response").html("Brilliant choice!");
-} else {
-  $("#response").html("Don’t you want a burrito?");
+let tipCalculator;
+tipCalculator = function(total, tipRate){
+  // step 1:
+  let tipAmount;
+  tipAmount = tipRate * total;
+  // and step 2:
+  $("#response").html("Your tip is $" + tipAmount);
 }
+
+// Now call (or “execute”) the function, passing a
+// total of $50.00 and a tipRate of 20%:
+
+tipCalculator(50.00, 0.2);
