260e0d17acf569ccb5ec4f4974351936e0408a70
Vanessa Lu <vanessalu@Vanessas-MacBook-Pro.local>
2017-09-30 10:59:48 -0400

    lastone

--- scripts.js
+++ scripts.js
@@ -1,24 +1,5 @@
-let myHabaneroSauceSquirts, myBurritoObject;
-// First, define and assign a variable for how
-// spicy the burrito is.
-myHabaneroSauceSquirts = 3;
-// Now assign the burrito object.
-myBurritoObject = {
-  tortilla: "wheat",
-  guacamole: true,
-  beans: "pinto",
-  // Make use of the variable above.
-  habaneroSauceSquirts: myHabaneroSauceSquirts,
-  // Use the variable again in a function.
-  spiciness: function(){
-    if (myHabaneroSauceSquirts > 0 ){
-      alert("This is a spicy burrito!");
-    } else {
-      alert("This is a mild burrito.");
-    }
-  }
-};
-$("#response").html("Your burrito has " +
-  myBurritoObject.habaneroSauceSquirts +
-  " squirts of habanero.");
-myBurritoObject.spiciness();
+var arrayExample = [1,2,3,"four"]
+function theLastOne(arr) {
+   return arr[arr.length - 1];
+}
+alert(theLastOne([1,2,3,"four"]))
