a3ec0a6ec4e4562daff34aa6547403dfe9fc4a1e
Vanessa Lu <vanessalu@Vanessas-MacBook-Pro.local>
2017-10-24 20:07:54 -0400

    html hw

--- scripts.js
+++ scripts.js
@@ -1,8 +1,1 @@
-let turtlesWithSplinter, turtlesWithoutSplinter, reversedTurtlesWithoutSplinter;
-turtlesWithSplinter = ["Leonardo", "Donatello", "Raphael", "Michelangelo", "Splinter"];
-// oops. let's pop() Splinter off before reversing…
-turtlesWithoutSplinter = turtlesWithSplinter.pop();
-console.log(turtlesWithoutSplinter);
-//--> Splinter
-//reversedTurtlesWithoutSplinter = turtlesWithSplinter.pop().reverse();
-//$("#response").html(reversedTurtlesWithoutSplinter);;
+$( "p" ).last ().css( "color", "red" ).addClass("lead");
