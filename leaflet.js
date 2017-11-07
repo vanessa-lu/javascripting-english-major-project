a369a442d5d68d496e89ccadaba46fde0d0496fc
Vanessa Lu <vanessalu@Vanessas-MacBook-Pro.local>
2017-11-06 20:24:18 -0500

    polyline

--- leaflet.js
+++ leaflet.js
@@ -1,14 +1,27 @@
 let firstMap, tileLayer;
 firstMap = L.map("first-map");
 tileLayer =
-L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
-      attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy;<a href='http://carto.com/attribution'>CARTO</a>",
-      subdomains: "abcd",
-      maxZoom: 18
-    });
+  L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
+    attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy;<a href='http://carto.com/attribution'>CARTO</a>",
+    subdomains: "abcd",
+    maxZoom: 18
+  });
 tileLayer.addTo(firstMap);
 firstMap.setView([40.730833, -73.9975], 16);
-let bobstLibrary;
-bobstLibrary = L.marker([40.729444, -73.997222]);
-bobstLibrary.addTo(firstMap);
-bobstLibrary.bindPopup("This is Bobst Library.")
+let washingtonSquarePark, washingtonSquareParkMarker;
+let tenThousandth, tenThousandthPolygon, thousandth, thousandthPolyline;
+washingtonSquarePark = L.latLng(40.730833, -73.9975);
+washingtonSquareParkMarker = L.marker(washingtonSquarePark).addTo(firstMap);
+tenThousandth = [[40.7307, -73.9976], [40.7307, -73.9974],
+                 [40.7309, -73.9974], [40.7309, -73.9976]];
+tenThousandthPolygon = L.polygon(tenThousandth, {
+                                  color: "#268bd2",
+                                  fillColor: "#fdf6e3"
+                                }
+                        ).addTo(firstMap);
+                    thousandth = [[washingtonSquarePark.lat + 0.001, washingtonSquarePark.lng + 0.001],
+                                      [washingtonSquarePark.lat + 0.001, washingtonSquarePark.lng - 0.001],
+                                      [washingtonSquarePark.lat - 0.001, washingtonSquarePark.lng - 0.001],
+                                      [washingtonSquarePark.lat - 0.001, washingtonSquarePark.lng + 0.001]];
+                        thousandthPolyline = L.polyline(thousandth, {color: "#d33682"}
+                                              ).addTo(firstMap);
