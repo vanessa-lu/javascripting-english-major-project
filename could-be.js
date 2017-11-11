ce02dc149d5f6553e5030dae3116f729d1e86f4d
Vanessa Lu <vanessalu@Vanessas-MacBook-Pro.local>
2017-11-11 14:36:30 -0500

    could be project

---
+++ could-be.js
@@ -0,0 +1,8 @@
+let map, tileLayer;
+map = L.map("could-be-map");
+tileLayer = L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png", {
+              attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> &copy; <a href='http://carto.com/attribution'>CARTO</a>",
+              subdomains: "abcd",
+              maxZoom: 18
+            }).addTo(map);
+map.setView([40.730833, -73.9975], 16);
