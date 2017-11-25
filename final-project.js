1e49eafd5c0828b4a6830e96862453a6481a955a
Vanessa Lu <vanessalu@Vanessas-MacBook-Pro.local>
2017-11-25 12:25:07 -0500

    final project sections

--- final-project.js
+++ final-project.js
@@ -113,16 +113,20 @@ data = {
   map.zoomOut(1);
 
   // Define and assign a Markdown-it renderer.
-let md;
-md = window.markdownit({html: true}).use(window.markdownitFootnote);
-// Load the Markdown file with jQuery.
-$.ajax({
-  url: "https://vanessa-lu.github.io/javascripting-english-major-project//final-project.md",
-  success: function(markdown){
-    // Convert the Markdown to HTML.
-    let html;
-    html = md.render(markdown);
-    // Print the HTML to #content using jQuery.
-    $("#content").html(html);
-  }
-});
+  let md;
+  md = window.markdownit({html: true}).use(window.markdownitFootnote);
+  ["Upper_West_Side", "Columbus",
+    "Columbia_University", "Sydney",
+    "Dansville", "Watertown"].forEach(function(tab){
+    // Create a variable tab that has the name as a string.
+    $.ajax({
+      // tab + ".md" yields, for example, "rampart.md".
+      url: tab + ".md",
+      success: function(markdown){
+        let html;
+        html = md.render(markdown);
+        // "#rampart", for example.
+        $("#" + tab).html(html);
+      }
+    });
+  });
